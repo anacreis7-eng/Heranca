62d78e169ae412ede75ac0a4d719fdd4250a55cc
anacreis7-eng <ana.c.reis7@edu.senai.br>
2026-09-11 09:47:26 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro meuCachorro = new Cachorro("Jade", "Vira-Lata");
+            Gato meuGato = new Gato("Mingau", "Vira-Lata");
+
+            meuCachorro.Comer();
+            meuCachorro.Latir();
+            meuGato.Comer();
+            meuGato.Miar();
+;        }
+    }
+}
