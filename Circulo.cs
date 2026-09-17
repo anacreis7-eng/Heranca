32e54d83729599c78b850da0e5a3837b944d47e6
anacreis7-eng <ana.c.reis7@edu.senai.br>
2026-09-17 09:36:25 -0300

    Alterações 2

--- Circulo.cs
+++ Circulo.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace Heranca
 {
-    internal class Circulo
+    internal class Circulo : FormaGeometrica
     {
+        public double raio { get; set; }
+        public override double CalcularArea()
+        {
+            return Math.PI * raio * raio;
+
+        }
     }
 }
